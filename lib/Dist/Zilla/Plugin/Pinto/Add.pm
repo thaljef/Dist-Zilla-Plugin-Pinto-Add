ca0b86f6bb5c4c0e6d6bb7e14503c628e7cc5788
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-01-22 04:36:44 -0800

    You can now release to multiple repositories.
    This feature was inspired by reneeb!

--- lib/Dist/Zilla/Plugin/Pinto/Add.pm
+++ lib/Dist/Zilla/Plugin/Pinto/Add.pm
@@ -7,10 +7,11 @@ use Moose::Util::TypeConstraints;
 
 use English qw(-no_match_vars);
 
-use MooseX::Types::Moose qw(Str);
+use MooseX::Types::Moose qw(Str ArrayRef);
 use Pinto::Types qw(AuthorID);
 
 use Class::Load qw();
+use Try::Tiny;
 
 #------------------------------------------------------------------------------
 
@@ -27,11 +28,13 @@ class_type('Pinto::Remote');
 
 #------------------------------------------------------------------------------
 
-has repos => (
-    is        => 'ro',
-    isa       =>  Str,
-    required  => 1,
+has root => (
+    is         => 'ro',
+    isa        => ArrayRef[Str],
+    auto_deref => 1,
+    required   => 1,
 );
+
 
 has author => (
     is         => 'ro',
@@ -39,10 +42,12 @@ has author => (
     lazy_build => 1,
 );
 
-has pinto => (
+
+has pintos => (
     is         => 'ro',
-    isa        => 'Pinto | Pinto::Remote',
+    isa        => ArrayRef['Pinto | Pinto::Remote'],
     init_arg   => undef,
+    auto_deref => 1,
     lazy_build => 1,
 );
 
@@ -51,44 +56,44 @@ has pinto => (
 sub _build_author {
     my ($self) = @_;
 
-    my $author = $self->_get_pause_id() || $self->_get_username()
-       || $self->log_fatal('Unable to determine your author ID');
-
-    return $author;
+    return $self->_get_pause_id()
+           || $self->_get_username()
+           || $self->_prompt_for_author_id();
 }
 
 #------------------------------------------------------------------------------
 
-sub _build_pinto {
+sub _build_pintos {
     my ($self) = @_;
 
-    my $repos = $self->repos();
-    my $type  = $repos =~ m{^ http:// }mx ? 'remote'        : 'local';
-    my $class = $type eq 'remote'         ? 'Pinto::Remote' : 'Pinto';
     my $version = $self->VERSION();
     my $options = { -version => $version };
+    my @pintos  = ();
 
-    $self->log_fatal("You must install $class-$version to release to a $type repository: $@")
-        if not eval { Class::Load::load_class($class, $options); 1 };
+    for my $root ($self->root) {
+        my $type  = $root =~ m{^ http:// }mx ? 'remote'        : 'local';
+        my $class = $type eq 'remote'        ? 'Pinto::Remote' : 'Pinto';
 
-    return $class->new(repos => $repos, quiet => 1);
+
+        $self->log_fatal("You must install $class-$version to release to a $type repository: $@")
+            if not eval { Class::Load::load_class($class, $options); 1 };
+
+        my $pinto = try   { $class->new(root => $root, quiet => 1) }
+                    catch { $self->log_fatal($_) };
+
+        push @pintos, $self->_ping_it($pinto) ? $pinto : ();
+    }
+
+    $self->log_fatal('none of your repositories are available') if not @pintos;
+    return \@pintos;
 }
 
 #------------------------------------------------------------------------------
 
-sub release {
-    my ($self, $archive) = @_;
+sub _ping_it {
+    my ($self, $pinto) = @_;
 
-    return $self->_ping() && $self->_release($archive);
-}
-
-#------------------------------------------------------------------------------
-
-sub _ping {
-    my ($self) = @_;
-
-    my $pinto = $self->pinto();
-    my $root = $pinto->config->root();
+    my $root  = $pinto->root();
     $self->log("checking if repository at $root is available");
 
     $pinto->new_batch(noinit => 1);
@@ -98,32 +103,35 @@ sub _ping {
 
     my $msg = "repository at $root is not available.  Abort the rest of the release?";
     my $abort  = $self->zilla->chrome->prompt_yn($msg, {default => 'Y'});
-    $self->log_fatal('Aborting') if $abort;
-
+    $self->log_fatal('Aborting') if $abort; # dies!
     return 0;
 }
 
 #------------------------------------------------------------------------------
 
-sub _release {
+sub mvp_multivalue_args { qw(root) }
+
+#------------------------------------------------------------------------------
+
+sub release {
     my ($self, $archive) = @_;
 
-    my $pinto = $self->pinto();
-    my $root  = $pinto->config->root();
-    $self->log("adding $archive to repository at $root");
+    for my $pinto ( $self->pintos() ) {
 
-    $pinto->new_batch();
-    $pinto->add_action('Add', author => $self->author(), archive => $archive);
-    my $result = $pinto->run_actions();
+        my $root  = $pinto->root();
+        $self->log("adding $archive to repository at $root");
 
-    if ($result->is_success()) {
-        $self->log("added $archive ok");
-        return 1;
+        $pinto->new_batch();
+        $pinto->add_action('Add', author => $self->author(), archive => $archive);
+        my $result = $pinto->run_actions();
+
+        $result->is_success() ? $self->log("added $archive to $root ok")
+                              : $self->log_fatal("failed to add $archive to $root: $result");
+
+        # TODO: Should we try to release to all pintos, even if one fails?
     }
-    else {
-        $self->log_fatal("failed to add $archive: " . $result->to_string() );
-        return 0;
-    }
+
+    return 1;
 }
 
 #------------------------------------------------------------------------------
@@ -150,8 +158,18 @@ sub _get_username {
         return uc $name;
     }
 
-    # TODO: prompt?
     return;
+}
+
+#------------------------------------------------------------------------------
+
+sub _prompt_for_author_id {
+    my ($self) = @_;
+
+    my $msg = 'What is your author ID?';
+    my $id  = uc $self->zilla->chrome->prompt_str->($msg);
+
+    return $id;
 }
 
 #------------------------------------------------------------------------------
@@ -166,7 +184,7 @@ __END__
 
   # In your dist.ini
   [Pinto::Add]
-  repos  = http://pinto.my-company.com  ; required
+  root   = http://pinto.my-host         ; at lease one root is required
   author = YOU                          ; optional. defaults to username
 
   # Then run the release command
@@ -195,13 +213,17 @@ distribution:
 
 =over 4
 
-=item repos = REPOSITORY
+=item root = REPOSITORY
 
-This identifies the Pinto repository you want to release to.  If
-C<REPOSITORY> looks like a URL (i.e. starts with "http://") then your
-distribution will be shipped with L<Pinto::Remote>.  Otherwise, the
-C<REPOSITORY> is assumed to be a path to a local repository directory.
-In that case, your distribution will be shipped with L<Pinto>.
+This identifies the root of the Pinto repository you want to release
+to.  If C<REPOSITORY> looks like a URL (i.e. starts with "http://")
+then your distribution will be shipped with L<Pinto::Remote>.
+Otherwise, the C<REPOSITORY> is assumed to be a path to a local
+repository directory.  In that case, your distribution will be shipped
+with L<Pinto>.
+
+At least one C<root> is required.  You release to mutiple repositories
+by specifying the C<root> attribute multiple times.
 
 =item author = NAME
 
